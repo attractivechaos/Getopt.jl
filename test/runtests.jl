47febae5b9a92a55b60bfe6e7f35fdb25b2ad9e3
Attractive Chaos <attractor@live.co.uk>
2018-09-03 15:13:32 -0400

    added more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,11 +2,19 @@ using Getopt
 using Test
 
 @testset "Getopt" begin
-	args = ["-xxy1", "--foo", "arg1", "--bar=2"]
-	count = 0
+	args = ["-xxy1", "-y", "22", "--foo", "arg1", "-", "--wrong", "--bar=2", "--", "-x"]
+	count, missing, n_x, l_y = 0, 0, 0, 0
 	for (opt, arg) in Getopt.getopt(args, "xy:", ["foo", "bar="])
-		if opt != "?" count += 1 end
+		if opt != "?" count += 1
+		else missing += 1
+		end
+		if opt == "-x" n_x += 1
+		elseif opt == "-y" l_y += length(arg)
+		end
 	end
-	@test count == 5
-	@test length(args) == 1
+	@test count == 6
+	@test length(args) == 3
+	@test missing == 1
+	@test n_x == 2
+	@test l_y == 3
 end
